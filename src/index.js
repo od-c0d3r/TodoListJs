ae2018577ef2a35118250c0d970ed08181f32000
od-c0d3r <omar_rashad@aol.com>
2021-12-02 14:19:47 +0200

    Repo: install local linters

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
 
 document.body.appendChild(component());
