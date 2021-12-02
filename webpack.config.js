ed3708c0c51155f5d9922891b0e8960ca033da5b
od-c0d3r <omar_rashad@aol.com>
2021-12-02 17:57:08 +0200

    App: Display list array

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
