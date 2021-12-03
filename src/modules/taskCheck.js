6a74a00a5441fb1270db098ac0f3a453418b9406
od-c0d3r <omar_rashad@aol.com>
2021-12-03 11:38:59 +0200

    Feat: Add check functionality

--- src/modules/taskCheck.js
+++ src/modules/taskCheck.js
@@ -6,4 +6,18 @@ function hi2() {
     console.log('2nd function from taskCheck')
 }
 
-export {hi,hi2}
+function checkboxListener() {
+    document.addEventListener('click', e => {
+        if (e.target.type === "checkbox" ) {
+            if (e.target.checked == true) {
+                console.log(true);
+            } else {
+                console.log(false)
+            }
+        }
+    })
+}
+
+checkboxListener()
+
+export {hi, hi2}
