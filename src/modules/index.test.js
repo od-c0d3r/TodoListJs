3a22ee77d536daa019ce7bee2a82271180055bff
Anselem <anselemodims@gmail.com>
2021-12-08 13:08:02 +0100

    Added a function to test the first index of the result

--- src/modules/index.test.js
+++ src/modules/index.test.js
@@ -11,14 +11,17 @@ describe('Add and Remove', ()=> {
             comp: false,
             index: arr.length + 1
         };
-    
+        
+        add(task);
         const result = add(task);
         
+        const first = add(task)[0];
         expect(getTasksFromLocalStorage()).toEqual([]);
         expect(setTasksToLocalStorage(arr)).toEqual({list: arr});
         expect(result).toEqual([obj])
         expect(result.length).toBe(1)
         expect(result[0].index).toBe(1)
+        expect(first).toEqual(obj)
         expect(typeof(result)).toEqual('object')
         expect(typeof(result[0].comp)).toEqual('boolean')
         expect(typeof(result[0].index)).toEqual('number')
